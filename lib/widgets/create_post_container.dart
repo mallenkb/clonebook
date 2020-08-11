b1a257b2fdaba7370a983d5aad663caaf36fe69f
mallenkb <40618900+mallenkb@users.noreply.github.com>
2020-08-11 17:11:23 +0000

    Removed Unsed Package

--- lib/widgets/create_post_container.dart
+++ lib/widgets/create_post_container.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_responsive_ui/models/user_model.dart';
 import 'package:flutter_facebook_responsive_ui/models/models.dart';
